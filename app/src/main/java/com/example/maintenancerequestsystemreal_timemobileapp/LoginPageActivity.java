43a3aa8c4e52ece2c56156cd2c41bd03ed67dad5
Nat Nats <vargasnatnat10@gmail.com>
2026-03-19 16:12:38 +0800

    Added LOGIN AND SIGN UP PAGE USING REALTIME DATABASE VIA FIREBASEDATABASE

--- app/src/main/java/com/example/maintenancerequestsystemreal_timemobileapp/LoginPageActivity.java
+++ app/src/main/java/com/example/maintenancerequestsystemreal_timemobileapp/LoginPageActivity.java
@@ -1,24 +1,113 @@
 package com.example.maintenancerequestsystemreal_timemobileapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class LoginPageActivity extends AppCompatActivity {
+
+    EditText login_userName, login_userPassword;
+    Button loginButton;
+    TextView signUpRedirectPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        login_userName = findViewById(R.id.username);
+        login_userPassword = findViewById(R.id.password);
+        loginButton = findViewById(R.id.btnLogin);
+        signUpRedirectPage = findViewById(R.id.signUpRedirectPage);
+
+
+        loginButton.setOnClickListener(v -> {
+            if (!validateUsername() | !validatePassword()) {
+
+            } else {
+                checkUser();
+            }
+        });
+
+        signUpRedirectPage.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginPageActivity.this, SignUpPageActivity.class);
+            startActivity(intent);
+        });
+
+    }
+
+    public Boolean validateUsername() {
+        String val = login_userName.getText().toString();
+        if (val.isEmpty()) {
+            login_userName.setError("Username cannot be empty");
+            return false;
+        } else {
+            login_userName.setError(null);
+            return true;
+        }
+    }
+
+    public Boolean validatePassword() {
+        String val = login_userPassword.getText().toString();
+        if (val.isEmpty()) {
+            login_userPassword.setError("Password cannot be empty");
+            return false;
+        } else {
+            login_userPassword.setError(null);
+            return true;
+        }
+    }
+
+    public void checkUser() {
+        String userName = login_userName.getText().toString();
+        String password = login_userPassword.getText().toString();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase = reference.orderByChild("username").equalTo(userName);
+
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    login_userName.setError(null);
+                    String passwordFromDb = snapshot.child(userName).child("password").getValue(String.class);
+
+                    if (!Objects.equals(passwordFromDb, password)) {
+                        login_userName.setError(null);
+                        Intent intent = new Intent (LoginPageActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        login_userPassword.setError("Invalid Credentials");
+                        login_userPassword.requestFocus();
+                    }
+                } else {
+                    login_userName.setError("User does not exist");
+                    login_userName.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
     }
 }
--- app/src/main/java/com/example/maintenancerequestsystemreal_timemobileapp/SignUpPageActivity.java
+++ app/src/main/java/com/example/maintenancerequestsystemreal_timemobileapp/SignUpPageActivity.java
@@ -1,6 +1,12 @@
 package com.example.maintenancerequestsystemreal_timemobileapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,51 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SignUpPageActivity extends AppCompatActivity {
+
+    EditText signupName, signupEmail, signupUsername, signupPassword;
+    TextView loginRedirectText;
+    Button signupButton;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        signupName = findViewById(R.id.signupName);
+        signupEmail = findViewById(R.id.signupEmail);
+        signupUsername = findViewById(R.id.signupUsername);
+        signupPassword = findViewById(R.id.signupPassword);
+        signupButton = findViewById(R.id.btnSignUp);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+
+        signupButton.setOnClickListener(v -> {
+            database = FirebaseDatabase.getInstance();
+            reference = database.getReference("users");
+
+
+            String name = signupName.getText().toString().trim();
+            String email = signupEmail.getText().toString().trim();
+            String username = signupUsername.getText().toString().trim();
+            String password = signupPassword.getText().toString().trim();
+            
+            HelperClassLoginSignup helperClass = new HelperClassLoginSignup(name, email, username, password);
+            reference.child(name).setValue(helperClass);
+
+            Toast.makeText(this, "Signed Up Successfully", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(SignUpPageActivity.this, LoginPageActivity.class);
+            startActivity(intent);
+        });
+
+        loginRedirectText.setOnClickListener(v -> {
+            Intent intent = new Intent(SignUpPageActivity.this, LoginPageActivity.class);
+            startActivity(intent);
         });
     }
 }
