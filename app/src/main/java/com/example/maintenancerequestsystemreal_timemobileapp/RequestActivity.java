0767d8532d7cc7eb1ba9ee248cf6aa7028b79269
Nat Nats <vargasnatnat10@gmail.com>
2026-05-14 21:58:54 +0800

    Update RequestActivity.java

--- app/src/main/java/com/example/maintenancerequestsystemreal_timemobileapp/RequestActivity.java
+++ app/src/main/java/com/example/maintenancerequestsystemreal_timemobileapp/RequestActivity.java
@@ -1,6 +1,9 @@
 package com.example.maintenancerequestsystemreal_timemobileapp;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +12,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class RequestActivity extends AppCompatActivity {
 
     Toolbar tb;
+    EditText etRequestTitle, etLocation, etDescription;
+    Button btnSubmitRequest, btnSelectImage;
+    DatabaseReference dataBaseReference;
+
+    Uri imageUri;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+
+    HelperClassRequest helperClassRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +39,21 @@ public class RequestActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         tb = findViewById(R.id.toolbar);
-
         tb.setNavigationOnClickListener(v -> finish());
+
+        etRequestTitle = findViewById(R.id.etRequestTitle);
+        etLocation = findViewById(R.id.etLocation);
+        etDescription = findViewById(R.id.etDescription);
+        btnSubmitRequest = findViewById(R.id.btnSubmitRequest);
+        btnSelectImage = findViewById(R.id.btnUploadImage);
+
+        
+
+
+
+
+
+
     }
 }
